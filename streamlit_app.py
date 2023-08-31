6883b8a225f2b2f4a598f6a3fef98e651381fb61
Karunaaggarwal <64397177+Karunaaggarwal@users.noreply.github.com>
2023-08-31 20:06:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,23 @@ def find_largest(num1, num2, num3):
   
 def main():
   st.title("Find the Largest Number")
+
+  st.title(
+    """
+    <style>
+    .reportview-container {
+        background-color: #f0f5f5;
+    }
+    .sidebar .sidebar-content {
+        background-color: #2ecc71;
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
   num1 = st.number_input("Enter the First number:")
   num2 = st.number_input("Enter the Second number:")
   num3 = st.number_input("Enter the Third number:")
